8ff09b3424853fe793f569912eb6c5df221647ce
Peter Mbanugo <peter@foobar.agency>
2021-08-14 19:32:58 +0200

    add slash command

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const commands = require("probot-commands");
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
@@ -11,6 +13,11 @@ module.exports = (app) => {
       body: "Thanks for opening this issue!",
     });
     return context.octokit.issues.createComment(issueComment);
+  });
+
+  commands(app, "kind", (context, command) => {
+    const labels = command.arguments.split(/, */);
+    return context.octokit.issues.addLabels(context.issue({ labels }));
   });
 
   // For more information on building apps:
